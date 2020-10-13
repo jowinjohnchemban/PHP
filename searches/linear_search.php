1c9c65bc5f653a0e7203a55dd312896be91f8088
Jowin John Chemban <29167718+jowinjohnchemban@users.noreply.github.com>
2020-10-13 11:35:08 +0530

    Update linear_search.php

--- searches/linear_search.php
+++ searches/linear_search.php
@@ -8,31 +8,46 @@
  * @param Array $list a array of integers to search
  * @param integer $target an integer number to search for in the list
  * @return integer the index where the target is found (or -1 if not found)
+ *
+ *
+ * USAGE: linear_search(ARRAY,INTEGER);
  * 
+ * OUTPUT: 0        - If not found
+ *         POSITION - If found
+ *
  * Examples:
  * 
- *  data =  5, 7, 8, 11, 12, 15, 17, 18, 20
+ *  linear_search([5, 7, 8, 11, 12, 15, 17, 18, 20], 15)
+ *  6
  *  
- *  x = 15
- *  Element found at position 6
- *  
- *  x = 1
- *  Element not found
+ *  linear_search([5, 7, 8, 11, 12, 15, 17, 18, 20], 1)
+ *  0
  * 
  **/
 
 
 function linear_search($list, $target) #Linear Search
-  { $n = sizeof($list); 
+  { $n = sizeof($list);
     for($i = 0; $i < $n; $i++) 
     { 
-        if($list[$i] == $target) 
-            return $i+1; 
+        if($list[$i] == $target)
+                {
+                  return ($i+1); #FOUND
+                }
     } 
-    return -1; 
+    return 0;  # NOT FOUND
    }
-   
-# DRIVER CODE
+
+
+
+# DRIVER CODE  
+
+echo linear_search([5, 7, 8, 11, 12, 15, 17, 18, 20], 15); # OUTPUT DISPLAY | FOUND
+echo linear_search([5, 7, 8, 11, 12, 15, 17, 18, 20], 1);  # OUTPUT DISPLAY | NOT FOUND
+
+
+# Example
+#/*
 $data = array(5, 7, 8, 11, 12, 15. 17, 18, 20);  
 $x = 15; 
 
@@ -42,5 +57,5 @@ if($result == -1)
     echo "Element not found"; 
 else
     echo "Element found at position " , $result; 
-
+#*/
 ?>
